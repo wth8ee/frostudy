3385a1f2937bb748f98626cf06cc20d7098cc092
wth8ee <wth8eeyv2@gmail.com>
2026-08-06 18:12:30 +0300

    auth fix 2

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,6 +1,6 @@
-import { PrismaClient } from "@prisma/client";
 import { Pool } from "pg";
 import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "@prisma/client/extension";
 
 const connectionString = `${process.env.DATABASE_URL}`;
 const pool = new Pool({ connectionString });
