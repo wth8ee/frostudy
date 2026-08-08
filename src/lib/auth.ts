bd83ba9fbbeb36292d6d6c4e385421b86e306e49
wth8ee <wth8eeyv2@gmail.com>
2026-08-08 14:49:48 +0300

    fixes

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -14,6 +14,10 @@ export const auth = betterAuth({
   emailAndPassword: {
     enabled: true,
   },
-  trustedOrigins: ["https://vercel.app", `https://${process.env.VERCEL_URL}`],
+  trustedOrigins: [
+    "http://localhost:3000",
+    ...(process.env.VERCEL_URL ? [`https://${process.env.VERCEL_URL}`] : []),
+    ...(process.env.NEXT_PUBLIC_APP_URL ? [process.env.NEXT_PUBLIC_APP_URL] : []),
+  ],
   plugins: [nextCookies()],
 });
